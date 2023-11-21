83d69c89e2bcacb756c98b72b40f613836d1073e
Felipe Amorim <felipeamorim.go@gmail.com>
2023-11-21 04:56:39 -0300

    feat: navigate adicionado no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,29 @@
 import { MapPin, ShoppingCart } from '@phosphor-icons/react'
 import { Aside, CartButton, CartFeedback, HeaderContainer } from './styles'
+import { useContext } from 'react'
+import { cartContext } from '../../context/CartContext'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 export function Header() {
+  const { cartList } = useContext(cartContext)
+  const qntItensInCart = cartList.filter((item) => item.quantity > 0)
+  const navigate = useNavigate()
   return (
     <HeaderContainer>
-      <img src="./logo.svg" alt="um copo de café roxo" />
+      <NavLink to="/">
+        <img src="./logo.svg" alt="um copo de café roxo" />
+      </NavLink>
+
       <Aside>
         <div>
           <MapPin weight="fill" color="#8047F8" size={22} className="teste" />
           <span>Porto Alegre, RS</span>
         </div>
-        <CartButton>
+        <CartButton onClick={() => navigate('cart')}>
           <ShoppingCart weight="fill" color="#C47F17" size={19} />
-          <CartFeedback>8</CartFeedback>
+          {qntItensInCart.length > 0 ? (
+            <CartFeedback>{qntItensInCart.length}</CartFeedback>
+          ) : null}
         </CartButton>
       </Aside>
     </HeaderContainer>
