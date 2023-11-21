4cedeb65f178716d0612b75947bc49a373f5860f
Felipe Amorim <felipeamorim.go@gmail.com>
2023-11-20 21:09:29 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Heart } from '@phosphor-icons/react'
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello World
+        <Heart weight="fill" />
+      </h1>
+    </div>
+  )
+}
+
+export default App
