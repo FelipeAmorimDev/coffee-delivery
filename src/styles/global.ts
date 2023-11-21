74cda20f899d0b78e5185d82293ea4187706532d
Felipe Amorim <felipeamorim.go@gmail.com>
2023-11-20 21:57:52 -0300

    feat: default theme e o css global foram criados

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+import { mixins } from './mixins'
+
+export const GlobalStyle = createGlobalStyle`
+ * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button{
+    ${mixins.fonts.textM}
+  }
+`
