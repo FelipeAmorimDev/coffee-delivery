7d4f3a233129d8b9b2ad7ff7e95546042d11c5e2
Felipe Amorim <felipeamorim.go@gmail.com>
2023-11-24 02:56:18 -0300

    feat: ações do reducer agora são feitas com immer

--- src/reducers/cart/reducer.ts
+++ src/reducers/cart/reducer.ts
@@ -1,89 +1,90 @@
-import { IOrders } from '../../context/CartContext'
-import { IItemToAdd } from '../../pages/Home'
 import { ActionTypes } from './actions'
+import { produce } from 'immer'
+
+export interface ICoffee {
+  id: string
+  title: string
+  description: string
+  tags: string[]
+  price: string
+  image: string
+  quantity: number
+}
+
+export interface IOrder {
+  id: number
+  zip: string
+  address: string
+  number: string
+  complement: string
+  neighborhood: string
+  city: string
+  state: string
+  paymentMethod: string
+}
 
 interface ICart {
-  cartList: IItemToAdd[]
-  orderList: IOrders[]
+  cartList: ICoffee[]
+  orderList: IOrder[]
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function cartReducer(state: ICart, action: any) {
-  if (action.type === ActionTypes.ADD_ITEM_TO_CART) {
-    const itemExistInCart = state.cartList.find(
-      (item) => item.id === action.payload.itemToAdd.id,
-    )
-    if (itemExistInCart) {
-      const addOneToExistingItem = state.cartList.map((cart) => {
-        if (cart.id === itemExistInCart.id) {
-          return {
-            ...cart,
-            quantity: cart.quantity + action.payload.itemToAdd.quantity,
-          }
+  switch (action.type) {
+    case ActionTypes.ADD_ITEM_TO_CART: {
+      return produce(state, (draft) => {
+        const itemExistInCart = draft.cartList.find(
+          (item) => item.id === action.payload.itemToAdd.id,
+        )
+
+        if (itemExistInCart) {
+          itemExistInCart.quantity += action.payload.itemToAdd.quantity
+        } else {
+          draft.cartList.push(action.payload.itemToAdd)
         }
-        return cart
+      })
+    }
+
+    case ActionTypes.CLEAR_CART_LIST:
+      return produce(state, (draft) => {
+        draft.cartList.splice(0)
       })
 
-      return { ...state, cartList: [...addOneToExistingItem] }
-    } else {
-      return {
-        ...state,
-        cartList: [...state.cartList, action.payload.itemToAdd],
-      }
-    }
-  }
+    case ActionTypes.REMOVE_ITEM_IN_CART:
+      return produce(state, (draft) => {
+        const coffeeItem = draft.cartList.findIndex(
+          (coffee) => coffee.id === action.payload.id,
+        )
+        draft.cartList.splice(coffeeItem, 1)
+      })
 
-  if (action.type === ActionTypes.CLEAR_CART_LIST) {
-    return {
-      ...state,
-      cartList: [],
-    }
-  }
-
-  if (action.type === ActionTypes.REMOVE_ITEM_IN_CART) {
-    return {
-      ...state,
-      cartList: state.cartList.filter(
-        (cartItem) => cartItem.id !== action.payload.id,
-      ),
-    }
-  }
-
-  if (action.type === ActionTypes.DECREASE_ITEM_QUANTITY) {
-    return {
-      ...state,
-      cartList: state.cartList.map((cartItem) => {
-        if (cartItem.id === action.payload.id) {
-          if (cartItem.quantity > 1) {
-            return { ...cartItem, quantity: cartItem.quantity - 1 }
-          } else {
-            return cartItem
-          }
-        } else {
-          return cartItem
+    case ActionTypes.DECREASE_ITEM_QUANTITY: {
+      return produce(state, (draft) => {
+        const coffeeItem = draft.cartList.find(
+          (coffee) => coffee.id === action.payload.id,
+        )
+        if (coffeeItem) {
+          const quantity = coffeeItem.quantity
+          coffeeItem.quantity = quantity > 1 ? quantity - 1 : quantity
         }
-      }),
+      })
     }
-  }
 
-  if (action.type === ActionTypes.INCREASE_ITEM_QUANTITY) {
-    return {
-      ...state,
-      cartList: state.cartList.map((cartItem) => {
-        if (cartItem.id === action.payload.id) {
-          return { ...cartItem, quantity: cartItem.quantity + 1 }
-        } else {
-          return cartItem
+    case ActionTypes.INCREASE_ITEM_QUANTITY:
+      return produce(state, (draft) => {
+        const coffeeItem = draft.cartList.find(
+          (coffee) => coffee.id === action.payload.id,
+        )
+        if (coffeeItem) {
+          coffeeItem.quantity = coffeeItem.quantity + 1
         }
-      }),
-    }
-  }
+      })
 
-  if (action.type === 'CHECKOUT') {
-    return {
-      ...state,
-      orderList: [...state.orderList, { ...action.payload.newOrder }],
-    }
-  }
+    case ActionTypes.CHECKOUT:
+      return produce(state, (draft) => {
+        draft.orderList.push(action.payload.newOrder)
+      })
 
-  return state
+    default:
+      return state
+  }
 }
